4860d8c99074ec42c299bbf1736b307749fe5253
Unknown <dliberman@itba.edu.ar>
2017-10-06 21:34:54 -0300

    pageAllocator should be working

---
+++ Kernel/PageAllocator/pageAllocator.h
@@ -0,0 +1,11 @@
+#ifndef PAGEALLOCATOR_H
+#define PAGEALLOCATOR_H
+
+#include <stdint.h>
+
+#define CONCURRENT_ALLOCATED_PAGES 20
+#define MAX_PHYSICAL_PAGES 0x4000 //asumimos como máximo 32 GB (2^14) de memoria RAM
+#define PAGE_SIZE 2 //2 MB
+#define USED_PAGE 1
+#define LAST_KERNEL_PAGE 9
+#endif
