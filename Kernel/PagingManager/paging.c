307f0031025b6cfbcd96b4100d24666f0c3bd5ad
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2017-10-07 16:09:42 -0300

    Reload of CR3 on context switch

--- Kernel/PagingManager/paging.c
+++ Kernel/PagingManager/paging.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <lib.h>
 
 #define USERLAND_LOGIC_PAGE 255
 
@@ -13,4 +14,10 @@ void mapUserspace(uint64_t pageAddr)
   // numero 10 de la PD.
   uint64_t pageNum = pageAddr/0x200000;
   PDAddr[USERLAND_LOGIC_PAGE] = PDAddr[pageNum];
+	reloadCR3();
+}
+
+uint64_t getLogicalUserlandPage()
+{
+	return USERLAND_LOGIC_PAGE*0x200000;
 }
